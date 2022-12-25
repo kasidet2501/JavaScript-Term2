cafd91a4663ce863585f21b85f6359993be8c718
gasidet2501 <s6406021622141@email.kmutnb.ac.th>
2022-12-25 20:08:01 +0700

    update

---
+++ Lecture-02/reverse2.js
@@ -0,0 +1,8 @@
+function reverseString(value) {
+
+    const reversedValue = value.split('').reverse().join('');
+    return reversedValue;
+    
+}
+    
+console.log(reverseString("Hello JavaScript"));
