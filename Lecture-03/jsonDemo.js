a37c9ef747be24e1080ac9fe1e5f7565c0163adb
gasidet2501 <s6406021622141@email.kmutnb.ac.th>
2022-12-25 20:44:56 +0700

    update

---
+++ Lecture-03/jsonDemo.js
@@ -0,0 +1,31 @@
+/*************************************
+
+    JavaScript for Beginners
+    JS101 ~ JSON Demo
+
+    BASICS      : JSON Format
+    STRINGIFY   : Javascript to JSON
+    PARSE       : JSON to JavaScript
+*************************************/
+
+
+// Think of @ simple JavaScript object with data (property values)
+const book = new Object({ title : "1984", author: "George Orwelt"});
+console.log("\n ------- ");
+
+console.log(typeof book);
+console.log(book);
+
+ 
+
+ 
+
+ 
+
+
+// You can atso have a collection of these objects
+// const myBooks = [
+// new Object({ title : "1984", author: "George Orwell"),
+// new Object({ title : "Becoming", author: “Michelle Obama"}),
+// new Object({ title : "Snow Crash", author: "Neal Stephenson"}),
+// new Object({ title : "Predictably Irrational", author: "Dan Ariely"})
---
+++ Lecture-03/mapReformatArray.js
@@ -0,0 +1,10 @@
+const kvArray = [
+    { key: 1, value: 10 },
+    { key: 2, value: 20 },
+    { key: 3, value: 30 },
+];
+    
+const reformattedArray = kvArray.map(({ key, value }) => ({ [key]: value }));
+
+console.log(reformattedArray); // [{ 1: 10 }, { 2: 20 }, { 3: 30 }]
+console.log(kvArray) ;
