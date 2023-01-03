549b1190f634c2947e59df15339592cc86d663bb
gasidet2501 <s6406021622141@email.kmutnb.ac.th>
2023-01-03 22:43:53 +0700

    update

---
+++ Lecture-02/Ex-01.js
@@ -0,0 +1,6 @@
+
+const findPerimeter = (num1,num2) => (num1+num2)*2
+
+console.log(findPerimeter(6,7));
+console.log(findPerimeter(20,10));
+console.log(findPerimeter(2,9));
---
+++ Lecture-02/Ex-02.js
@@ -0,0 +1,5 @@
+const equation = (str) => eval(str)
+
+console.log(equation("1+1"))
+console.log(equation("7*4-2"))
+console.log(equation("1+1+1+1+1"))
