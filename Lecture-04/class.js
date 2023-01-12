c1b40f880c63bca0a7a9b9dc0ba2edb132c45210
mikey2501 <mikeysoggy@gmail.com>
2023-01-12 13:12:27 +0700

    update

---
+++ Lecture-04/class.js
@@ -0,0 +1,14 @@
+class Song{
+  constructor() {
+    this.title;
+    this.author;
+  }
+  play() {
+    console. log("Song playing!");
+  }
+}
+
+    const mySong = new Song();
+    mySong.play();
+
+
---
+++ Lecture-04/libHello.js
@@ -0,0 +1,19 @@
+// VibHeLlo. js
+const person ={
+name: 'Anirach',
+weight: 85
+}
+
+function sayHello(){
+  console.log("Hello World!")
+}
+
+function cube(x){
+return X*X*X;
+}
+
+const add =(a,b) => a+b;
+
+ const status = true;
+
+module.exports = {person, sayHello, cube, add, status}
